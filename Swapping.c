d84dbf4ce51465aec428edf9eb62d1c3d97ed8e4
Madhurya <85580048+Madhurya-3@users.noreply.github.com>
2023-08-12 17:56:45 +0530

    Created Swapping
    Swaps two numbers

---
+++ Swapping.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,temp;
+	printf("Enter 'a' value : ");
+	scanf("%d",&a);
+	printf("Enter 'b' value : ");
+	scanf("%d",&b);
+	temp=a;
+	a=b;
+	b=temp;
+	printf("After Swapping, ");
+	printf("a = %d ",a);
+	printf("b = %d",b);
+}
